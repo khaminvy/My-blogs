e984c5e5a471d3326f74c9f184f7608f453d0588
Thanh Pham <khanhthy@yahoo.com>
2023-11-14 22:19:11 -0800

    Created function getPostData(id) in lid/posts and used in [id].js and updated component Post

--- pages/posts/[id].js
+++ pages/posts/[id].js
@@ -1,9 +1,26 @@
 import Layout from '../../components/layout';
-import { getAllPostIds } from '../../lib/posts';
+import { getAllPostIds, getPostData } from '../../lib/posts';
 
-export default function Post() {
-  return <Layout>...</Layout>;
+export async function getStaticProps({ params }) {
+  const postData = getPostData(params.id);
+  return {
+    props: {
+      postData,
+    },
+  };
 }
+
+export default function Post({ postData }) {
+    return (
+      <Layout>
+        {postData.title}
+        <br />
+        {postData.id}
+        <br />
+        {postData.date}
+      </Layout>
+    );
+  }
 
 export async function getStaticPaths() {
   // Return a list of possible value for id
